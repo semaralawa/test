4447d3723a3620557de854013a98b332ef57df7f
dewasanta2 <dewasanta2@gmail.com>
2021-12-05 21:21:22 +0800

    edit how to connect to websocket

--- client/webrtc.js
+++ client/webrtc.js
@@ -20,12 +20,18 @@ function pageReady() {
   remoteVideo = document.getElementById('remoteVideo');
 
   address = window.location.hostname;
-  if (address.includes('localhost')) {
-    serverConnection = new WebSocket('wss://' + address + ':8443');
-  }
-  else {
-    serverConnection = new WebSocket('wss://' + window.location.hostname);
-  }
+
+  //use this if you want to add some domain
+  // if (address.includes('localhost')) {
+  //   serverConnection = new WebSocket('wss://' + address + ':8443');
+  // }
+  // else {
+  //   serverConnection = new WebSocket('wss://' + window.location.hostname);
+  // }
+
+  //comment this if you use code above it
+  serverConnection = new WebSocket('wss://' + address + ':8443');
+
   serverConnection.onmessage = gotMessageFromServer;
 
   var constraints = {
